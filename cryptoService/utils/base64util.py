eaa10068e0b3dd9e3c31c3b7cf3d4e809010f857
Thanh Nguyen <nnnannn710@gmail.com>
2025-01-11 01:37:57 +0700

    Initial commit

--- cryptoService/utils/base64util.py
+++ cryptoService/utils/base64util.py
@@ -1,6 +1,8 @@
 import hashlib
 import base64
 
+# Using SHA-256 (Secure Hash Algorithm 256-bit) for hashing 
+# Encoding the result in Base64 (with URL-safe encoding)
 
 def convertStringToBase64String(init_key):
     hashed_key = hashlib.sha256(init_key.encode()).digest()[:32]
